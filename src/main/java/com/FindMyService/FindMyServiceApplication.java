aa2c2de2c5e8ec80fcad4fbbe64f9d534d672bd3
Hritam Kar <hritam.kar@sap.com>
2025-11-18 19:58:21 +0530

    First commit
    First Commit

---
+++ src/main/java/com/FindMyService/FindMyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.FindMyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FindMyService/FindMyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FindMyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
